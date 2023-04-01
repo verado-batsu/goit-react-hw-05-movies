077cd7ea02b1c56ab758fcdece17b78d3b01a3d3
verado-batsu <77026004+verado-batsu@users.noreply.github.com>
2023-04-01 10:08:56 +0000

    Deploying to gh-pages from @ verado-batsu/goit-react-hw-05-movies@304cdca010417e524c95178e2afa084fb672561d 🚀

---
+++ static/js/216.c785b952.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[216],{216:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(87),o=r(184);function u(){return(0,o.jsxs)("div",{children:["NotFound. Please go to the ",(0,o.jsx)(n.rU,{to:"/",children:"home page"})]})}}}]);
+//# sourceMappingURL=216.c785b952.chunk.js.map
